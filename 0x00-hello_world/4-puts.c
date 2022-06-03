0edfba10f022b65548b23a34863fc444e8bf0911
estherchukwuekezie <estherchukwuekezie>
2022-06-03 03:19:29 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 { 
-	puts("\"programming is like building a multilingual puzzle);
+	char test_str{} = "\"programming is like building a multilingual puzzle";
+	puts(test_str);
 	return (0);
 }
